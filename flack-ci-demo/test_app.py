eba299f02b4092fed732cbc5bf823c6a3930191a
BhanuTejaReddy <157331824+Bhanuteja11@users.noreply.github.com>
2025-09-19 21:02:19 +0530

    Update test_app.py

--- flack-ci-demo/test_app.py
+++ flack-ci-demo/test_app.py
@@ -1,8 +1,18 @@
+# from app import app
+
+# def test_home():
+#     client = app.test_client()
+#     response = client.get("/")
+#     assert response.data == b"Hello, CI/CD!"
+#     assert response.status_code == 200
+#     print("1")
 from app import app
 
 def test_home():
     client = app.test_client()
     response = client.get("/")
+    print("Response data:", response.data)        # <-- prints actual response
+    print("Status code:", response.status_code)  # <-- prints HTTP status
     assert response.data == b"Hello, CI/CD!"
     assert response.status_code == 200
-    print("1")
+
